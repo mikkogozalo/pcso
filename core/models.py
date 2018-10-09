cf44bc5629bf1f6f607f944e6f85e07259648747
Mikko Gozalo <mikgozalo@gmail.com>
2018-10-09 19:59:46 +0800

    Update models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,6 @@
+from itertools import chain
+from collections import Counter
+
 from django.db import models
 
 
@@ -9,6 +12,17 @@ class Game(models.Model):
     pcso_game_id = models.PositiveSmallIntegerField(db_index=True)
     digits = models.PositiveSmallIntegerField(default=6)
     max_digit = models.PositiveSmallIntegerField(default=58)
+
+    def find_draws(self, numbers: str):
+        numbers = [int(_) for _ in numbers.split('-')]
+        draws_with_numbers = []
+        for n in numbers:
+            draws_with_numbers.append(Draw.objects.filter(balls__ball=n).distinct().values_list('id',
+                                                                                                           flat=True))
+        draws_with_numbers = chain(*draws_with_numbers)
+        ball_counters = Counter(draws_with_numbers)
+        winning = {k: v for k, v in ball_counters.items() if v >= 5}
+        return winning
 
 
 class Draw(models.Model):
